f7159b07d411a40d594d40197d8d9b640963a6cc
Geoffrey Bachelet <grosfrais@gmail.com>
2014-08-02 21:59:13 +0200

    removed deadcode in UserRepository

--- src/App/Model/UserRepository.php
+++ src/App/Model/UserRepository.php
@@ -3,10 +3,8 @@
 namespace App\Model;
 
 use Doctrine\ORM\EntityRepository;
-use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
-class UserRepository extends EntityRepository implements UserProviderInterface
+class UserRepository extends EntityRepository
 {
     public function findOneBySpec($spec)
     {
@@ -15,30 +13,5 @@ class UserRepository extends EntityRepository implements UserProviderInterface
         }
 
         return $this->findOneByUsername($spec);
-    }
-
-    public function findOneByGithubUsername($username)
-    {
-        return $this->findOneByUsername($username);
-    }
-
-    public function loadUserByUsername($username) {
-        return $this->getEntityManager()
-            ->createQuery('SELECT u FROM
-                Model:User u
-                WHERE u.username = :username
-                OR u.email = :username')
-            ->setParameters(array(
-                'username' => $username
-            ))
-            ->getOneOrNullResult();
-    }
-
-    public function refreshUser(UserInterface $user) {
-        return $this->loadUserByUsername($user->getUsername());
-    }
-
-    public function supportsClass($class) {
-        return $class === 'App\Model\User';
     }
 }
