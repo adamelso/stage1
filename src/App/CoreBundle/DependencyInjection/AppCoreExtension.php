1d5908680aac077422da4824b052c975e4fef279
Geoffrey Bachelet <grosfrais@gmail.com>
2014-08-03 15:59:39 +0200

    removed controllers.xml service configuration

--- src/App/CoreBundle/DependencyInjection/AppCoreExtension.php
+++ src/App/CoreBundle/DependencyInjection/AppCoreExtension.php
@@ -20,7 +20,7 @@ class AppCoreExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config/services'));
-        $files = ['controllers', 'form', 'listeners', 'misc', 'model', 'rabbitmq', 'repositories', 'twig', 'providers'];
+        $files = ['form', 'listeners', 'misc', 'model', 'rabbitmq', 'repositories', 'twig', 'providers'];
 
         foreach ($files as $file) {
             $loader->load(sprintf('%s.xml', $file));
